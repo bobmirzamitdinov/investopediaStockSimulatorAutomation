58cbac8802f7cfb44a62a3dca9db9a88b1edb5d9
bobmirzamitdinov <bobmirzamitdinov@gmail.com>
2023-10-08 16:52:26 -0500

    Updated stepDefinitions for login feature

--- src/test/java/stepDefinitions/loginSteps.java
+++ src/test/java/stepDefinitions/loginSteps.java
@@ -1,38 +1,62 @@
 package stepDefinitions;
 
-import java.util.concurrent.TimeUnit;
-
+import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class loginSteps {
 
     WebDriver driver = null;
-    @Given("user is on login page")
+    private Properties properties;
+    String projectPath = System.getProperty("user.dir");
+    public loginSteps() {
+        properties = new Properties();
+        try {
+            FileInputStream inputStream = new FileInputStream(projectPath + "/src/test/resources/config.properties");
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Given("browser is open")
+    public void browser_is_open() {
+
+        System.getProperty("webdriver.chrome.driver",projectPath + "/src/test/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+        driver.manage().window().maximize();
+    }
+    @And("user is on login page")
     public void user_is_on_login_page() {
-        System.out.println("12");
-        throw new io.cucumber.java.PendingException();
+        driver.navigate().to("https://www.investopedia.com/auth/realms/investopedia/protocol/openid-connect/auth?client_id=finance-simulator&redirect_uri=https%3A%2F%2Fwww.investopedia.com%2Fsimulator%2Fportfolio&state=00a02e9c-05cf-416d-8c07-2c59e671880a&response_mode=fragment&response_type=code&scope=openid&nonce=17f9e3a2-2101-41ae-a0d2-925d589f6b80");
     }
-    @When("user enters username and password")
-    public void user_enters_username_and_password() {
-        System.out.println("123");
-        throw new io.cucumber.java.PendingException();
+    @When("user enters email address and password")
+    public void user_enters_email_address_and_password() {
+        String email = properties.getProperty("email");
+        String password = properties.getProperty("password");
+
+        driver.findElement(By.name("username")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys(password);
     }
-    @When("clicks on login button")
-    public void clicks_on_login_button() {
-        System.out.println("1234");
-        throw new io.cucumber.java.PendingException();
+    @And("clicks on sign in button")
+    public void clicks_on_sign_in_button() {
+        driver.findElement(By.name("login")).sendKeys(Keys.ENTER);
     }
-    @Then("user is navigated to the home page")
-    public void user_is_navigated_to_the_home_page() {
-        System.out.println("12345");
-        throw new io.cucumber.java.PendingException();
+    @Then("user is navigated to the portfolio page")
+    public void user_is_navigated_to_the_portfolio_page() {
+        driver.getPageSource().contains("OVERVIEW");
+        driver.close();
+        driver.quit();
     }
+
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,7 +1,8 @@
 Feature: Test login functionality
 
   Scenario: Check login is successful using valid credentials
-    Given user is on login page
-    When user enters username and password
-    And clicks on login button
-    Then user is navigated to the home page
+    Given browser is open
+    And user is on login page
+    When user enters email address and password
+    And clicks on sign in button
+    Then user is navigated to the portfolio page
