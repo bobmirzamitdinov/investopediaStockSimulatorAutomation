5c1822ca9eb69869cd9fab34ab78ed3a0a47e9b6
bobmirzamitdinov <bobmirzamitdinov@gmail.com>
2023-10-13 20:19:37 -0500

    Updated two steps in Research feature

--- src/test/java/stepDefinitions/researchSteps.java
+++ src/test/java/stepDefinitions/researchSteps.java
@@ -1,5 +1,5 @@
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+package stepDefinitions;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -13,10 +13,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class researchSteps {
     WebDriver driver = null;
-    private Properties properties;
+    private final Properties properties;
     String projectPath = System.getProperty("user.dir");
 
     public researchSteps() {
@@ -31,9 +32,15 @@ public class researchSteps {
 
     @Given("user is on the investopedia simulator website")
     public void user_is_on_the_investopedia_simulator_website() {
+        System.getProperty("webdriver.chrome.driver",projectPath + "/src/test/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        driver.manage().window().maximize();
         driver.navigate().to("https://www.investopedia.com/auth/realms/investopedia/protocol/openid-connect/auth?client_id=finance-simulator&redirect_uri=https%3A%2F%2Fwww.investopedia.com%2Fsimulator%2Fportfolio&state=00a02e9c-05cf-416d-8c07-2c59e671880a&response_mode=fragment&response_type=code&scope=openid&nonce=17f9e3a2-2101-41ae-a0d2-925d589f6b80");
     }
-
     @And("user logs in with valid credentials")
     public void user_logs_in_with_valid_credentials() {
         String email = properties.getProperty("email");
@@ -41,19 +48,20 @@ public class researchSteps {
 
         driver.findElement(By.name("username")).sendKeys(email);
         driver.findElement(By.name("password")).sendKeys(password);
+
+        driver.findElement(By.name("login")).sendKeys(Keys.ENTER);
     }
 
     @When("user navigates to the Research page")
     public void user_navigates_to_the_research_page() {
-        driver.findElement(By.name("login")).sendKeys(Keys.ENTER);
+        WebElement researchLink = driver.findElement(By.cssSelector("a[data-cy='global-nav-item']"));
+        researchLink.click();
     }
-
     @And("user searches for the company")
     public void user_searches_for_the_company() {
         driver.findElement(By.name("search")).sendKeys(Keys.ENTER);
 
     }
-
     @Then("user verifies that the results contain company>")
     public void user_verifies_that_the_results_contain_keyword(io.cucumber.datatable.DataTable dataTable) {
         driver.getPageSource().contains("Apple");
